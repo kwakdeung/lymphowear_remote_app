8ec014d005ece3fdaa8aebdd144b2c8bdd7e0bb8
kwakdeung <51012589a@gmail.com>
2022-07-27 08:54:23 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/lymphowear_remote_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lymphowear_remote_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/lib_color_schemes.g.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+// Meterial 3 Theme Color
+// Meterial 3 Theme Builder(Color 설정): https://material-foundation.github.io/material-theme-builder/
+
+const seed = Color(0xFF51A48F);
+
+const lightColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Color(0xFF006B57),
+  onPrimary: Color(0xFFFFFFFF),
+  primaryContainer: Color(0xFF7BF8D9),
+  onPrimaryContainer: Color(0xFF002019),
+  secondary: Color(0xFF4B635C),
+  onSecondary: Color(0xFFFFFFFF),
+  secondaryContainer: Color(0xFFCDE8DE),
+  onSecondaryContainer: Color(0xFF081F1A),
+  tertiary: Color(0xFF426277),
+  onTertiary: Color(0xFFFFFFFF),
+  tertiaryContainer: Color(0xFFC6E7FF),
+  onTertiaryContainer: Color(0xFF001E2E),
+  error: Color(0xFFBA1B1B),
+  errorContainer: Color(0xFFFFDAD4),
+  onError: Color(0xFFFFFFFF),
+  onErrorContainer: Color(0xFF410001),
+  background: Color(0xFFFAFDFA),
+  onBackground: Color(0xFF191C1B),
+  surface: Color(0xFFFAFDFA),
+  onSurface: Color(0xFF191C1B),
+  surfaceVariant: Color(0xFFDBE5E0),
+  onSurfaceVariant: Color(0xFF3F4945),
+  outline: Color(0xFF6F7975),
+  onInverseSurface: Color(0xFFEFF2EF),
+  inverseSurface: Color(0xFF2D312F),
+  inversePrimary: Color(0xFF5CDBBD),
+  shadow: Color(0xFF000000),
+);
+
+const darkColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFF5CDBBD),
+  onPrimary: Color(0xFF00382C),
+  primaryContainer: Color(0xFF005141),
+  onPrimaryContainer: Color(0xFF7BF8D9),
+  secondary: Color(0xFFB1CCC2),
+  onSecondary: Color(0xFF1D352E),
+  secondaryContainer: Color(0xFF334C44),
+  onSecondaryContainer: Color(0xFFCDE8DE),
+  tertiary: Color(0xFFA9CBE3),
+  onTertiary: Color(0xFF0F3447),
+  tertiaryContainer: Color(0xFF294A5E),
+  onTertiaryContainer: Color(0xFFC6E7FF),
+  error: Color(0xFFFFB4A9),
+  errorContainer: Color(0xFF930006),
+  onError: Color(0xFF680003),
+  onErrorContainer: Color(0xFFFFDAD4),
+  background: Color(0xFF191C1B),
+  onBackground: Color(0xFFE0E3E0),
+  surface: Color(0xFF191C1B),
+  onSurface: Color(0xFFE0E3E0),
+  surfaceVariant: Color(0xFF3F4945),
+  onSurfaceVariant: Color(0xFFBEC9C4),
+  outline: Color(0xFF89938F),
+  onInverseSurface: Color(0xFF191C1B),
+  inverseSurface: Color(0xFFE0E3E0),
+  inversePrimary: Color(0xFF006B57),
+  shadow: Color(0xFF000000),
+);
