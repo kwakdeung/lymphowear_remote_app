2aeb4de2cdd74be1666dd0538e1556aa95e98123
kwakdeung <51012589a@gmail.com>
2022-08-11 13:27:32 +0900

    responsible

--- lib/components/lymphowear_state.dart
+++ lib/components/lymphowear_state.dart
@@ -13,8 +13,7 @@ class LymphoWearState extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              margin:
-                  const EdgeInsets.fromLTRB(53, 20, 53, 8), // 113, 20, 113, 8
+              margin: const EdgeInsets.fromLTRB(53, 0, 53, 8),
               child: const FittedBox(
                 child: Text(
                   'Manual Mode',
@@ -22,48 +21,50 @@ class LymphoWearState extends StatelessWidget {
                 ),
               ),
             ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(48, 0, 48, 24),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Container(
-                  //   margin: const EdgeInsets.fromLTRB(0, 96, 0, 16),
-                  //   child: ElevatedButton(
-                  //     onPressed: () {},
-                  //     style: ElevatedButton.styleFrom(
-                  //         side: const BorderSide(color: Colors.black),
-                  //         shape: const CircleBorder(), //<-- SEE HERE
-                  //         padding: const EdgeInsets.all(10),
-                  //         surfaceTintColor: Colors.white),
-                  //     child: const Icon(
-                  //       Icons.remove,
-                  //       color: Colors.black,
-                  //       size: 20,
-                  //     ),
-                  //   ),
-                  // ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                    child: const CircularIndicator(),
-                  ),
-                  // Container(
-                  //   margin: const EdgeInsets.fromLTRB(0, 96, 0, 16),
-                  //   child: ElevatedButton(
-                  //     onPressed: () {},
-                  //     style: ElevatedButton.styleFrom(
-                  //         side: const BorderSide(color: Color(0xffE0E0E0)),
-                  //         shape: const CircleBorder(), //<-- SEE HERE
-                  //         padding: const EdgeInsets.all(10),
-                  //         surfaceTintColor: Colors.white),
-                  //     child: const Icon(
-                  //       Icons.add,
-                  //       color: Color(0xffE0E0E0),
-                  //       size: 20,
-                  //     ),
-                  //   ),
-                  // ),
-                ],
+            FittedBox(
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 96, 0, 16),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            side: const BorderSide(color: Colors.black),
+                            shape: const CircleBorder(),
+                            padding: const EdgeInsets.all(10),
+                            surfaceTintColor: Colors.white),
+                        child: const Icon(
+                          Icons.remove,
+                          color: Colors.black,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                      child: const CircularIndicator(),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.fromLTRB(0, 96, 0, 16),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            side: const BorderSide(color: Color(0xffE0E0E0)),
+                            shape: const CircleBorder(),
+                            padding: const EdgeInsets.all(10),
+                            surfaceTintColor: Colors.white),
+                        child: const Icon(
+                          Icons.add,
+                          color: Color(0xffE0E0E0),
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
