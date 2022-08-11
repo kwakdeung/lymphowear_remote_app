a23920955171a73eaa8ec34618c296c2544ea27e
kwakdeung <51012589a@gmail.com>
2022-08-11 15:26:12 +0900

    My commit message

--- lib/widgets/home/home_default.dart
+++ lib/widgets/home/home_default.dart
@@ -4,7 +4,6 @@ import 'package:lymphowear_remote_app/components/collarbone.dart';
 import 'package:lymphowear_remote_app/components/heating_option.dart';
 import 'package:lymphowear_remote_app/components/lymphowear_state.dart';
 import 'package:lymphowear_remote_app/components/shoulder.dart';
-import 'package:lymphowear_remote_app/constants.dart';
 
 class HomeDefault extends StatelessWidget {
   const HomeDefault({Key? key}) : super(key: key);
@@ -39,106 +38,16 @@ class HomeDefault extends StatelessWidget {
         ],
       ),
       body: Container(
+        width: double.infinity,
+        height: double.infinity,
         color: const Color(0xffDCDCDC),
         child: SingleChildScrollView(
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                if (isMobileWidth(context))
-                  if (isMobileHeight(context))
-                    Container(
-                      margin: const EdgeInsets.fromLTRB(20, 16, 20, 16),
-                      padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(color: Colors.black12, width: 0),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.7),
-                            spreadRadius: 0,
-                            blurRadius: 4.0,
-                            offset: const Offset(0, 4),
-                          ),
-                        ],
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FittedBox(
-                            child: GestureDetector(
-                                onTap: () {
-                                  debugPrint("Vital mode 홈페이지로 이동");
-                                },
-                                child: Container(
-                                  width: 107.0,
-                                  padding: const EdgeInsets.fromLTRB(
-                                      10.0, 0, 10.0, 0),
-                                  color: Colors.transparent,
-                                  child: Column(children: const [
-                                    Icon(
-                                      Icons.abc,
-                                    ),
-                                    Text(
-                                      "Vital\n Mode",
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(),
-                                    ),
-                                  ]),
-                                )),
-                          ),
-                          verticalDivider(),
-                          FittedBox(
-                            child: GestureDetector(
-                                onTap: () {
-                                  debugPrint("Relaxing mode 홈페이지로 이동");
-                                },
-                                child: Container(
-                                  width: 104.0,
-                                  padding: const EdgeInsets.fromLTRB(
-                                      10.0, 0, 10.0, 0),
-                                  color: Colors.transparent,
-                                  child: Column(children: const [
-                                    Icon(
-                                      Icons.abc,
-                                    ),
-                                    Text(
-                                      "Relaxingn\n Mode",
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(),
-                                    ),
-                                  ]),
-                                )),
-                          ),
-                          verticalDivider(),
-                          FittedBox(
-                            child: GestureDetector(
-                                onTap: () {
-                                  debugPrint("Slepping mode 홈페이지로 이동");
-                                },
-                                child: Container(
-                                  width: 107.0,
-                                  padding: const EdgeInsets.fromLTRB(
-                                      10.0, 0, 10.0, 0),
-                                  color: Colors.transparent,
-                                  child: Column(children: const [
-                                    Icon(
-                                      Icons.abc,
-                                    ),
-                                    Text(
-                                      "Slepping\n Mode",
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(),
-                                    ),
-                                  ]),
-                                )),
-                          ),
-                        ],
-                      ),
-                    ),
                 Container(
-                  margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  margin: const EdgeInsets.fromLTRB(20, 16, 20, 16),
                   padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(8),
@@ -153,35 +62,127 @@ class HomeDefault extends StatelessWidget {
                       ),
                     ],
                   ),
-                  child: Container(
-                    margin: const EdgeInsets.fromLTRB(20, 16, 20, 90),
-                    padding: const EdgeInsets.all(0.0),
-                    color: Colors.white,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.fromLTRB(0, 0, 0, 24),
-                          padding: const EdgeInsets.all(0.0),
-                          color: Colors.white,
-                          child: const LymphoWearState(),
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              border:
-                                  Border.all(color: Colors.black12, width: 2)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          debugPrint("Vital mode 홈페이지로 이동");
+                        },
+                        child: Container(
+                          width: 106.0,
+                          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                          color: Colors.transparent,
                           child: Column(
-                            children: const [
-                              Collarbone(),
-                              Armpit(),
-                              Shoulder(),
-                              HeatingOption(),
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                  margin: const EdgeInsets.fromLTRB(0, 8, 0, 4),
+                                  child: Image.asset(
+                                      'assets/images/ic_vital2.png')),
+                              const Text(
+                                "Vital\n Mode",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontSize: 10),
+                              ),
                             ],
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                      verticalDivider(),
+                      GestureDetector(
+                        onTap: () {
+                          debugPrint("Relaxing mode 홈페이지로 이동");
+                        },
+                        child: Container(
+                          width: 106.0,
+                          padding: const EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+                          color: Colors.transparent,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                  margin: const EdgeInsets.fromLTRB(0, 8, 0, 4),
+                                  child: Image.asset(
+                                      'assets/images/ic_relaxing2.png')),
+                              const Text(
+                                "Relaxing\n Mode",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontSize: 10),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      verticalDivider(),
+                      GestureDetector(
+                        onTap: () {
+                          debugPrint("Sleeping mode 홈페이지로 이동");
+                        },
+                        child: Container(
+                          width: 106.0,
+                          padding: const EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+                          color: Colors.transparent,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                  margin: const EdgeInsets.fromLTRB(0, 8, 0, 4),
+                                  child: Image.asset(
+                                      'assets/images/ic_sleeping2.png')),
+                              const Text(
+                                "Sleeping\n Mode",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(fontSize: 10),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(20, 16, 20, 16),
+                  padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(color: Colors.black12, width: 0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.7),
+                        spreadRadius: 0,
+                        blurRadius: 4.0,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(0, 0, 0, 24),
+                        padding: const EdgeInsets.all(0.0),
+                        color: Colors.white,
+                        child: const LymphoWearState(),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border:
+                                Border.all(color: Colors.black12, width: 2)),
+                        child: Column(
+                          children: const [
+                            Collarbone(),
+                            Armpit(),
+                            Shoulder(),
+                            HeatingOption(),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
