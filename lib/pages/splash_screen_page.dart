75c07f455229ad26896c6ae4f62b2ec5cbe341af
kwakdeung <51012589a@gmail.com>
2022-08-12 22:17:56 +0900

    Modify SplashScreen

--- lib/pages/splash_screen_page.dart
+++ lib/pages/splash_screen_page.dart
@@ -2,7 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:lymphowear_remote_app/main.dart';
+
+import 'package:lymphowear_remote_app/pages/onboarding_page.dart';
 
 class SplashScreenPage extends StatefulWidget {
   const SplashScreenPage({Key? key}) : super(key: key);
@@ -15,15 +16,13 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
   @override
   void initState() {
     // 부분 세로모드 고정하기 - SystemChrome.setPreferredOrientations를 initState에 세로고정 모드 삽입과 dispose에 원 상태 복귀 삽입
-    // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-    // Timer(const Duration(milliseconds: 3700), () {
-    //   // 시간 제한
-    //   Navigator.of(context).pushReplacement(MaterialPageRoute(
-    //     builder: (context) => const MyHomePage(
-    //       title: 'Test',
-    //     ),
-    //   )); // Navigator.of(context).pushReplacement(): 다음페이지인 onboarding page에서 back 버튼을 눌렀을 때 SplashScreen에 못넘어오게 제한
-    // });
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    Timer(const Duration(milliseconds: 3700), () {
+      // 시간 제한
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (context) => const OnboardingPage(),
+      )); // Navigator.of(context).pushReplacement(): 다음페이지인 onboarding page에서 back 버튼을 눌렀을 때 SplashScreen에 못넘어오게 제한
+    });
     super.initState();
   }
 
