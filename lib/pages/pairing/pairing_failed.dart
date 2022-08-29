bce0685b59338f5b0d4301677c2b77e78c34b433
kwakdeung <51012589a@gmail.com>
2022-08-29 09:44:31 +0900

    수정

--- lib/pages/pairing/pairing_failed.dart
+++ lib/pages/pairing/pairing_failed.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-import 'package:lymphowear_remote_app/pages/pairing/paring_reloading.dart';
+import 'package:lymphowear_remote_app/pages/pairing/pairing_reloading.dart';
 
 class PairingFailed extends StatefulWidget {
   const PairingFailed({Key? key}) : super(key: key);
