d2b24725e481e4284c8c34a6021454ad1156c1c6
kwakdeung <51012589a@gmail.com>
2022-08-31 17:28:54 +0900

    15분 타이머 수정중

--- lib/components/home/circularindicator.dart
+++ lib/components/home/circularindicator.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -13,32 +14,46 @@ class CircularIndicator extends StatefulWidget {
 class _CircularIndicatorState extends State<CircularIndicator>
     with TickerProviderStateMixin {
   late AnimationController controller;
-  double _value = 900;
+  bool isPlaying = false;
   bool _working = false;
-  String value = 'Paring...';
-  bool _active = true;
 
-  void _handleTap() {
-    if (_active) {
-      debugPrint('test true');
-      pairing();
-      startWorking();
-    } else {
-      debugPrint('test false');
-      pairing();
-      stopWorking();
-    }
-    setState(() {
-      _active = !_active;
+  String get countText {
+    Duration count = controller.duration! * controller.value;
+    return controller.isDismissed
+        ? '${(controller.duration!.inMinutes % 60).toString().padLeft(2, '0')}:${(controller.duration!.inSeconds % 60).toString().padLeft(2, '0')}'
+        : '${(count.inMinutes % 60).toString().padLeft(2, '0')}:${(count.inSeconds % 60).toString().padLeft(2, '0')}';
+  }
+
+  double progress = 1.0;
+
+  void notify() {
+    if (countText == '00:00') {}
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 900),
+    );
+    controller.reverse();
+    controller.addListener(() {
+      notify();
+      if (controller.isAnimating) {
+        setState(() {
+          progress = controller.value;
+        });
+      } else {
+        setState(() {
+          progress = 1.0;
+          isPlaying = false;
+        });
+      }
     });
   }
 
-  void pairing() async {
-    setState(() {
-      value;
-    });
-  }
-
+  // indicator 시작
   void startWorking() async {
     setState(() {
       _working = true;
@@ -50,7 +65,7 @@ class _CircularIndicatorState extends State<CircularIndicator>
       }
       await Future.delayed(const Duration(seconds: 1));
       setState(() {
-        _value -= 0.1;
+        _value -= 1;
       });
     }
     setState(() {
@@ -58,10 +73,17 @@ class _CircularIndicatorState extends State<CircularIndicator>
     });
   }
 
+  // indicator 멈춤
   void stopWorking() {
     setState(() {
       _working = false;
     });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -78,55 +100,95 @@ class _CircularIndicatorState extends State<CircularIndicator>
               fit: StackFit.expand,
               children: [
                 CircularProgressIndicator(
-                  value: _value,
+                  value: 0.0,
                   backgroundColor: Colors.green[100],
                   valueColor: const AlwaysStoppedAnimation<Color>(Colors.green),
                 ),
                 Center(
-                  child: FittedBox(
+                  child: Expanded(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Container(
-                          margin: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+                          margin: const EdgeInsets.fromLTRB(0, 14, 0, 7),
                           child: SvgPicture.asset(
                             'assets/images/Battery.svg',
                             width: 20,
                             height: 10,
                           ),
                         ),
-                        Text(
-                          "${(_value).round()}",
-                          style: const TextStyle(
-                            fontSize: 24,
-                            fontFamily: "Poppins",
+                        GestureDetector(
+                          onTap: () {
+                            if (controller.isDismissed) {
+                              showModalBottomSheet(
+                                context: context,
+                                builder: (context) => Container(
+                                  height: 150,
+                                  child: CupertinoTimerPicker(
+                                    initialTimerDuration: controller.duration!,
+                                    onTimerDurationChanged: (time) {
+                                      setState(() {
+                                        controller.duration = time;
+                                      });
+                                    },
+                                  ),
+                                ),
+                              );
+                            }
+                          },
+                          child: AnimatedBuilder(
+                            animation: controller,
+                            builder: (context, child) => Text(
+                              countText,
+                              style: const TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w400,
+                                  fontFamily: "Poppins"),
+                            ),
                           ),
                         ),
-                        Container(
-                          margin: const EdgeInsets.only(top: 8),
-                          width: 40.0,
-                          height: 40.0,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            border: Border.all(
-                                color: const Color(0xff0BB15D), width: 1.5),
-                            color: _active
-                                ? const Color(0xff0BB15D)
-                                : Colors.white,
-                          ),
-                          child: IconButton(
-                              onPressed: () {
-                                _handleTap();
-                              },
-                              color: _active
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 5, vertical: 10),
+                          child: Container(
+                            margin: const EdgeInsets.only(top: 0),
+                            width: 40.0,
+                            height: 40.0,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              border: Border.all(
+                                  color: const Color(0xff0BB15D), width: 1.5),
+                              color: isPlaying
                                   ? Colors.white
                                   : const Color(0xff0BB15D),
-                              icon: _active
-                                  ? SvgPicture.asset(
-                                      "assets/icons/ic_play_full.svg")
-                                  : SvgPicture.asset(
-                                      "assets/icons/ic_pause_full.svg")),
-                        ),
+                            ),
+                            child: IconButton(
+                                onPressed: () {
+                                  if (controller.isAnimating) {
+                                    controller.stop();
+                                    setState(() {
+                                      isPlaying = false;
+                                    });
+                                  } else {
+                                    controller.reverse(
+                                        from: controller.value == 0
+                                            ? 1.0
+                                            : controller.value);
+                                    setState(() {
+                                      isPlaying = true;
+                                    });
+                                  }
+                                },
+                                color: isPlaying
+                                    ? const Color(0xff0BB15D)
+                                    : Colors.white,
+                                icon: isPlaying
+                                    ? SvgPicture.asset(
+                                        "assets/icons/ic_pause_full.svg")
+                                    : SvgPicture.asset(
+                                        "assets/icons/ic_play_full.svg")),
+                          ),
+                        )
                       ],
                     ),
                   ),
@@ -150,7 +212,7 @@ class CircularIn {
   });
 }
 
-double _value = 0;
+double _value = 900;
 final List<CircularIn> circularinData = [
   CircularIn(
     value: _value,
