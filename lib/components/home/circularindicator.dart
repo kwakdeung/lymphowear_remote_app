5444964e37c02757cffa6926541fd0325f4502ac
kwakdeung <51012589a@gmail.com>
2022-09-06 16:18:40 +0900

    초기화면 수정중

--- lib/components/home/circularindicator.dart
+++ lib/components/home/circularindicator.dart
@@ -23,9 +23,18 @@ class _CircularIndicatorState extends State<CircularIndicator>
     // int seconds = (900.0 * controller.value).toInt();
     int seconds = (10.0 * controller.value).toInt();
 
+    var initstate = controller.duration!;
+
+    var initstatemin = (initstate.inSeconds ~/ 60).toString().padLeft(2, '0');
+
+    var initstatesec = (initstate.inSeconds % 60).toString().padLeft(2, '0');
+
     var min = (seconds ~/ 60).toString().padLeft(2, '0');
     var sec = (seconds % 60).toString().padLeft(2, '0');
-    return "$min:$sec";
+
+    return controller.isDismissed ? '$initstatemin:$initstatesec' : '$min:$sec';
+
+    // return "$min:$sec";
 
     // Duration count = controller.duration! * controller.value;
 
