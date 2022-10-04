a0240503016e61cb9776d68bf7f17488ae53e8d3
kwakdeung <51012589a@gmail.com>
2022-10-04 16:28:39 +0900

    Flutter_Local_Notification(Alarm) 기능 추가중
    *build.gradle: 29 라인 compileSdkVersion 33 변경
    
    *AndroidManifest.xml:
    3 라인 - 알림 권한 선언
    4 라인 -  기기를 종료하면 모든 알람이 취소방지
    5 라인 - 진동
    6 라인 - 화면이 꺼진 상태에서 CPU를 계속 실행하여 작업을 처리
    7 라인 - 전체 화면 인텐트에 대한 권한 변경
    31 라인 - BOOT_COMPLETED
    32 라인 - .MY_PACKAGE_REPLACED  나의 패키지 교체
    33 라인 - QUICKBOOT_POWERON
    
    *AppDelegate.swift:
    11~ 13 라인 Flutter_Local_Notification iOS 실행 추가
    
    *main.dart
    13 라인: 순서(전역 변수 수정할 계획)
    16 라인: 플러그인
    19 라인: model ReceivedNotification 불러오기
    24 라인: 알림의 주제 작성
    27 라인: Future async await 비동기 추가
    36 라인: 비동기 main 메소드 사용 시 추가 시 필요
    43 라인: 안드로이드 셋팅 - AndroidInitializationSettings
    49 라인: iOS 셋팅 - DarwinInitializationSettings
    72 라인: onDidReceiveNotificationResponse - notifacation 클릭 후 결과
    80라인: 현재 시간대를 얻기
    
    *received_notification.dart: ReceivedNotification 클래스 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart'; // 알림 구현
+import 'package:flutter_native_timezone/flutter_native_timezone.dart'; // 현지 시간 맞추기
 import 'package:lymphowear_remote_app/constants.dart';
 import 'package:lymphowear_remote_app/pages/splash_screen_page.dart';
+import 'package:lymphowear_remote_app/models/received_notification.dart';
+import 'package:rxdart/subjects.dart'; //  ReactiveX 기반의 Dart 언어 용 Reactive 프로그래밍 라이브러리
+import 'package:timezone/data/latest_all.dart' as tz; // 시간 설정
+import 'package:timezone/timezone.dart' as tz;
 
-void main() {
+// 순서
+int id = 0;
+
+// 알림 플러그인
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+// model
+final BehaviorSubject<ReceivedNotification> didReceiveLocalNotificationSubject =
+    BehaviorSubject<ReceivedNotification>();
+
+// 알림의 주제 작성
+final BehaviorSubject<String?> selectNotificationSubject =
+    BehaviorSubject<String?>();
+
+Future<void> main() async {
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarBrightness: Brightness.light,
       statusBarColor: Colors.white,
     ),
   );
+
+  // 비동기 main 메소드 사용 시 추가
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await _configureLocalTimeZone();
+
+// android - AndroidInitializationSettings
+// iOS - Darwin..., DarwinInitializationSettings
+  /// Android Permission Setting
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      // AndroidInitializationSettings('mipmap/ic_launcher');
+      // 안드로이드 알람 로고
+      AndroidInitializationSettings('app_icon');
+
+  /// iOS Permission Setting
+  final DarwinInitializationSettings initializationSettingsDarwin =
+      DarwinInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+    onDidReceiveLocalNotification:
+        (int id, String? title, String? body, String? payload) async {
+      didReceiveLocalNotificationSubject.add(
+        ReceivedNotification(
+          id: id,
+          title: title,
+          body: body,
+        ),
+      );
+    },
+  );
+
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsDarwin,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onDidReceiveNotificationResponse:
+          (NotificationResponse notificationResponse) {
+    // notifacation 클릭 후 결과 - AlertDialog 등 사용 가능
+  });
+
   runApp((const MyApp()));
+}
+
+Future<void> _configureLocalTimeZone() async {
+  tz.initializeTimeZones();
+  final String timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
+  // flutter_native_timezone - timezone의 현재 시간대를 얻는 방법을 제공
+  tz.setLocalLocation(tz.getLocation(timeZoneName));
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/models/received_notification.dart
@@ -0,0 +1,11 @@
+class ReceivedNotification {
+  final int id;
+  final String? title;
+  final String? body;
+
+  ReceivedNotification({
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+}
