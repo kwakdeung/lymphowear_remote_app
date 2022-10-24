4a418ba2b5c2ad134c69b33840650b29e1e510b9
kwakdeung <rhkrwnsdud22@naver.com>
2022-10-24 14:27:44 +0900

    splash description(설명) 변경
    *16, 21, 26 라인 설명 변경

--- lib/models/onboarding_model.dart
+++ lib/models/onboarding_model.dart
@@ -13,18 +13,16 @@ final List<Onboarding> onboardingData = [
     image: "assets/images/lymphowear_face.png",
     title: "Title title 1",
     description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "LymphoWear is the world-first wearable device for lymphatic drainage massage.",
   ),
   Onboarding(
     image: "assets/images/lymphowear_left.png",
     title: "Title title 2",
-    description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    description: "This app helps you to control your device easier.",
   ),
   Onboarding(
     image: "assets/images/lymphowear_face.png",
     title: "Title title 3",
-    description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    description: "Also enjoy 3 types of massage program we provide!",
   ),
 ];
